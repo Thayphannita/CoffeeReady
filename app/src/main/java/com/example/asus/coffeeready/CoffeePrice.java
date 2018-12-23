caca5fe0993b173d4fbb395836c6a77aa8b964da
khantey1998 <khanteylong@gmail.com>
2018-12-23 14:40:01 +0700

    update error

---
+++ app/src/main/java/com/example/asus/coffeeready/CoffeePrice.java
@@ -0,0 +1,17 @@
+package com.example.asus.coffeeready;
+
+/**
+ * Created by Tey on 21/12/18.
+ */
+
+public class CoffeePrice {
+    String price;
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+}
---
+++ app/src/main/java/com/example/asus/coffeeready/Product.java
@@ -0,0 +1,33 @@
+package com.example.asus.coffeeready;
+
+public class Product {
+    String name ,url,description;
+    public Product(){
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+}
